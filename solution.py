8d9db4f2140f2a9015525f15c541af626bae3610
andreimedved1123@gmail.com <amedved1@sheffiled.ac.uk>
2024-11-04 14:31:58 +0000

    Threaded result - Optimisation improvement?

--- solution.py
+++ solution.py
@@ -1,4 +1,5 @@
 import numpy as np
+import threading
 
 class WordSearch(object):
     ROW_LENGTH = 10000
@@ -8,40 +9,73 @@ class WordSearch(object):
         arr = np.array(list(grid))
         self.grid = arr.reshape(self.ROW_LENGTH, self.ROW_LENGTH)
 
-    def is_present(self, word):
+        self.found = False  
+        self.lock = threading.Lock()
+
+    def search_rows(self, word):
         word_len = len(word)
-
         for i in range(self.ROW_LENGTH):
+            if self.found:
+                return
             row = ''.join(self.grid[i, :])
-            col = ''.join(self.grid[:, i])
-
 
             while True:
-                #get indexes of the first letter of the word in the row and column
                 try:
                     index_row = row.index(word[0])
                 except ValueError:
                     index_row = -1
+                    break
+
+                if row[index_row:index_row + word_len] == word:
+                    #print(self.found)
+                    self.lock.acquire()
+                    self.found = True
+                    self.lock.release()
+                    #print(self.found)
+                    
+                row = row[index_row + 1:]
+
+    def search_columns(self, word):
+        word_len = len(word)
+        for i in range(self.ROW_LENGTH):
+            if self.found:
+                return
+            col = ''.join(self.grid[:, i])
+
+            while True:
                 try:
                     index_col = col.index(word[0])
                 except ValueError:
                     index_col = -1
-
-                if index_row == -1 and index_col == -1:
                     break
 
+                if col[index_col:index_col + word_len] == word:
+                    #print(self.found)
+                    self.lock.acquire()
+                    self.found = True
+                    self.lock.release()
+                    #print(self.found)
 
-                if (index_row != -1) and (row[index_row:index_row + word_len] == word):
-                    return True     
-                
-                if (index_col != -1) and (col[index_col:index_col + word_len] == word):
-                    return True
-                
-                row = row[index_row + 1:]
                 col = col[index_col + 1:]
 
+    def is_present(self, word):
+        self.found = False
 
-        return False
+        row_thread = threading.Thread(target=self.search_rows, args=(word,))
+        col_thread = threading.Thread(target=self.search_columns, args=(word,))
+
+        row_thread.start()
+        col_thread.start()
+        row_thread.join()
+        col_thread.join()
+        #print(col_thread.is_alive())
+        #print(row_thread.is_alive())
+
+        return self.found
+
+    
+
+
             #for j in range(self.ROW_LENGTH - word_len + 1):
             #    if (row[j:j + word_len] == word) or (col[j:j + word_len] == word):
             #        return True
